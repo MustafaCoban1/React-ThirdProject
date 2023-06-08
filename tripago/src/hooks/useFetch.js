ef120df7ca73ca7ef8cd5b9892b329acf76bd203
Metaflax <mstf.cobann1@gmail.com>
2023-06-08 16:18:35 +0300

    Project Completed
    Creating a custom Fetch Hook, Adding a Loading/Pending State, Cleanup Function, Aborting Fetch Request, useEffect Gotcha-Infinite Loops

--- tripago/src/hooks/useFetch.js
+++ tripago/src/hooks/useFetch.js
@@ -1,20 +1,44 @@
-import {useState, useEffect } from 'react'
+import {useState, useEffect, useRef } from 'react'
 
-const useFetch = (url) => {
+export const useFetch = (url, _options) => {
     const [data, setData] = useState(null)
-
-
+    const [isPending, setIsPending] = useState(false)
+    const [error, setError] = useState(null)
+    
+    const options = useRef(_options).current
 
     useEffect(() => {
+        console.log(options)
+        const controller = new AbortController()
+
         const fetchData = async () => {
-            const res = await fetch(url)
-            const json = await res.json()
+            setIsPending(true)
 
-            setData(json)
-        }
-
-    }, [url])
+            try {
+                const res = await fetch(url, { signal: controller.signal })
+                if (!res.ok) {
+                    throw new Error(res.statusText)
+                }
+                const json = await res.json()
     
-}
+                setIsPending(false)
+                setData(json)
+                setError(null)
+            } catch(err) {
+                if (err.name === "AbortError"){
+                    console.log('the fetch was aborted')
+                } else {
+                    setIsPending(false)
+                    setError('Could not fetch the data')
+                }
+            }
+        }
+        fetchData()
 
-export default useFetch
+        return () => {
+            controller.abort()
+        }
+    }, [url, options])
+
+    return { data, isPending, error }  
+}
